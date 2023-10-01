d1922756c610caf54efa06d98dc4417ede79bf01
Irwan Nawawi <muhammadirwannawawi@gmail.com>
2023-10-01 09:41:09 +0700

    create  dart dan menjalankannya

---
+++ dart_application_tgs/bin/dart_application_tgs.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_tgs/dart_application_tgs.dart' as dart_application_tgs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_tgs.calculate()}!');
+}
---
+++ dart_application_tgs/test/dart_application_tgs_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_tgs/dart_application_tgs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
