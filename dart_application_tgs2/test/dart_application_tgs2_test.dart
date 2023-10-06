7ccd3691d5b4b5cf1d21ac9d468de2daa19e35c1
Irwan Nawawi <muhammadirwannawawi@gmail.com>
2023-10-06 08:06:07 +0700

    membuat folder tugas 2

---
+++ dart_application_tgs2/test/dart_application_tgs2_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_tgs2/dart_application_tgs2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
