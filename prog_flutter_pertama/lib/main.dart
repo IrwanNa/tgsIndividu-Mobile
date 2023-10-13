9965b9f6cf4d5359623ca9d42352dd9c3eeb4ea2
Irwan Nawawi <muhammadirwannawawi@gmail.com>
2023-10-13 19:14:55 +0700

    memodifikasi isi page pertama flutter

--- prog_flutter_pertama/lib/main.dart
+++ prog_flutter_pertama/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Projek Flutter Pertama Saya',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Muhammad Irwan Nawawi'),
     );
   }
 }
@@ -106,6 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
+              'Selamat Datang di Aplikasi Mobile Pertama Saya'
               'You have pushed the button this many times:',
             ),
             Text(
